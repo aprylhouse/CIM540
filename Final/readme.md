2cd9c3f70e97b1f92de712df07c7be007469d246
aprylhouse <Apryl@Apryls-MacBook-Pro.local>
2017-04-19 21:07:51 -0400

    code plan, homework 5 and 8

--- Final/readme.md
+++ Final/readme.md
@@ -1,2 +1,34 @@
-IDEA I
+CODE PLAN
+For my final project I wish to make an interactive art piece of a scene turning from day to night. 
+
+INPUT
+    1. slider:
+        move slider right to turn the scene to night
+        move the slider left to return the scene to day
+    2. slider:
+        move slider right change the the music to night ambiance 
+        move slide left to change the music to day ambiance
+        
+        
+
+OUTPUT
+    1. 2D shapes:
+        change color 
+    2. sound:
+        change tune 
+
+PSUEDOCODE
+
+Slider (color)
+    requires p5.dom library 
+    when moved right changes colors of various objects to dark colors 
+    when moved left changes colors of various objects to bright colors
+    
+Slider (sound)
+    when moved right changes music to night sounds
+    when moved left changes music to daytime sounds 
+
+2D Shapes 
+    because there a multiple and many shapes that will make up the scene, I'm currenly not sure how many and which shapes will be a part of the overall design 
+    
     
---
+++ Homework 5/sketch.js
@@ -0,0 +1,74 @@
+var questions = ["Shakira is 5'4.",
+  "Shakira was born in which country?",
+  "How many languages can Shakira speak?",
+  "At what age did Shakira record her first album?",
+  "Who is Shakira's favorite actor?"
+];
+
+var choices = [
+  ["False", "True"],
+  ["Venzuela", "Colombia", "Mexico", "Brazil"],
+  ["1", "2", "3", "4", "5"],
+  ["13", "18", "22"],
+  ["Mel Gibson", "Brad Pitt", "Hugh Grant"]
+];
+
+var answers = ["False", "Colombia", "5", "13", "Hugh Grant"];
+
+var qCounter = 0;
+
+var currentAnswer = 0;
+
+var textArea = 100;
+
+function setup() {
+    createCanvas(1000, 600);
+
+}
+
+function draw() {
+  background(255);
+    textSize(30);
+  fill("black");
+    text("The Shakira Quiz", 500, 30);
+    textSize(18);
+  textAlign(CENTER);
+  text(questions[qCounter], 0, 100, width, 100);
+
+   for (var choice = 0; choice < choices[qCounter].length; choice++) {
+      //console.log(choices[0][choice]);
+      fill("black");
+      textAlign(CENTER);
+      text(choices[qCounter][choice], choice * textArea, height/2, textArea, textArea);
+      text(choice+1, choice * textArea, height/2 + 20, textArea, textArea);
+
+
+   }
+}
+
+function keyPressed() {
+  console.log(keyCode);
+
+  //49 == 1, 50 == 2, 51 == 3, 52 == 4
+  if(keyCode == 49){
+    currentAnswer = 0;
+  }else if(keyCode == 50){
+    currentAnswer = 1;
+  }else if(keyCode == 51){
+    currentAnswer = 2;
+  }else if(keyCode == 52){
+    currentAnswer = 3;
+  }
+
+  if(answers[qCounter] == choices[qCounter][currentAnswer]){
+    console.log("Correct Answer");
+  }else{
+    console.log("Wrong answer");
+  }
+
+  qCounter++
+  if (qCounter >= questions.length) {
+    qCounter = 0;
+  }
+}
+
